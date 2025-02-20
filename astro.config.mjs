249785ab71351e7a94d7c9b7a425ae13a3c24b3f
Paulo Henrique <pauloh1288@gmail.com>
2025-02-20 06:42:22 -0300

    feat: implement linkbio page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+// @ts-check
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://phenrique.me/',
-	integrations: [mdx(), sitemap()],
+  site: "https://www.phenrique.me",
 });
