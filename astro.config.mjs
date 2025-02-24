95483635d756acc71be05c6642312041a74d40a5
Paulo Henrique <pauloh1288@gmail.com>
2025-02-24 15:45:29 -0300

    feat: prepare to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
+import netlify from "@astrojs/netlify";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.phenrique.me",
+  site: "https://phenrique.me",
+  adapter: netlify(),
 });
