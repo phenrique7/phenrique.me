fe615d8c5bfb2c3dff31636250d6ad3a6ebb9c2b
Paulo Henrique <pauloh1288@gmail.com>
2025-05-28 11:26:39 -0300

    fix(locale): enhance language matching logic for `ensureChosenLanguage`
    Updated `ensureChosenLanguage` function to utilize `startsWith` for improved language matching. Adjusted return values for better consistency in locale mapping.

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,7 +1,13 @@
 import type { Languages } from "@/types/app";
 
 export function ensureChosenLanguage(chosenLanguage: string | undefined): Languages | undefined {
-  if (chosenLanguage === "en" || chosenLanguage === "pt" || chosenLanguage === "de") {
-    return chosenLanguage;
+  if (chosenLanguage?.startsWith("en")) {
+    return "en";
+  }
+  if (chosenLanguage?.startsWith("pt")) {
+    return "pt";
+  }
+  if (chosenLanguage?.startsWith("de")) {
+    return "de";
   }
 }
