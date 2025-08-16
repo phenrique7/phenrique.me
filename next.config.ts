5ae500cd3c79c0e2e281ad9e1d145c478f7063a4
Paulo Henrique <pauloh1288@gmail.com>
2025-08-16 13:11:26 -0300

    refactor(reading): replace Notion API with BaseHub queries and update related types and schemas

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "assets.basehub.com",
       },
-      {
-        protocol: "https",
-        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-      },
     ],
   },
 };
